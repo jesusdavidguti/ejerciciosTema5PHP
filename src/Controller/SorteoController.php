4db9c5d39baa405b4fa220d4a3a97774b5b6df98
usuario <jesusdavid.guti@gmail.com>
2021-03-02 13:53:18 +0100

    Ejercicio 5.3

--- src/Controller/SorteoController.php
+++ src/Controller/SorteoController.php
@@ -6,9 +6,50 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class SorteoController extends AbstractController
 {
+    public function euromillones()
+    {
+        $arr_estrellas = array();
+        $arr_numeros = array();
+        $i = 0;
 
+        do {
+            $numero = random_int(1, 50);
+
+            if (!in_array($numero, $arr_numeros, true)){
+                array_push($arr_numeros, $numero);
+                $i++;
+            }
+        } while ($i < 4);
+
+        sort($arr_estrellas);
+
+        $i = 0;
+        do {
+            $numero = random_int(1, 12);
+
+            if (!in_array($numero, $arr_estrellas, true)){
+                array_push($arr_estrellas, $numero);
+                $i++;
+            }
+        } while ($i < 3);
+
+        sort($arr_estrellas);
+
+        //$numero = random_int(0, $limite);
+
+        return $this->render('euromillones.html.twig', ['numero' => $i,
+                                                        'limite' => $i]);
+    }
+    
     public function numeroMaximo($maximo)
     {
+
+        /* if (!is_int($maximo)){
+            $maximo = 0;
+            return $this->redirectToRoute('app_numero_sorteo_max', 
+                                            array('maximo' => $maximo));
+        } */
+
         $numero = random_int(0, $maximo);
 
         return $this->render('numero.html.twig', ['numero' => $numero, 
@@ -32,6 +73,6 @@ class SorteoController extends AbstractController
 
     public function index()
     {
-        return $this->render('index.html.twig');
+        return $this->render('estructuraBase.html.twig');
     }
 }
